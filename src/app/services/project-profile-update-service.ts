46a8629a2c3c1ff3601537549e2a0808c5471ed7
MelvinRabang <mirabang_88@yahoo.com>
2018-10-09 14:31:22 +0800

    Refactor add PUT DELETE GET

--- src/app/services/project-profile-update-service.ts
+++ src/app/services/project-profile-update-service.ts
@@ -9,30 +9,30 @@ import { Observable } from 'rxjs/Observable';
 export class ProjectProfileUpdateService {
 
     private headers = new Headers({ 'Content-Type': 'application/json' });
-    private saveProjectProfileUrl = '/api/saveProjectProfiles';
-    private isProjectProfileExistUrl = '/api/isProjectProfileExist';
-    private deleteProjectProfileUrl = '/api/deleteProjectProfile';
-    private updateProjectProfileUrl = '/api/updateProjectProfile';
+    private projectProfileUrl = '/api/projectprofile';
 
     constructor(private http: Http) { }
 
     saveProjectProfile(projectUpdateProfile: ProjectProfile): Promise<ProjectProfile> {
-        return this.http.post(this.saveProjectProfileUrl, projectUpdateProfile).toPromise()
+        return this.http.post(this.projectProfileUrl, projectUpdateProfile).toPromise()
             .then(res => res.json() as ProjectProfile);
     }
 
     isProjectProfileDoesNotExist(projectUpdateProfile: ProjectProfile) : Promise<boolean> {
-        return this.http.post(this.isProjectProfileExistUrl, projectUpdateProfile).toPromise()
+        return this.http.post(this.projectProfileUrl, projectUpdateProfile).toPromise()
             .then(res => res.json() as boolean);
     }
 
     deleteProjectProfile(projectProfileDeleted: ProjectProfile) : Promise<number> {
-        return this.http.post(this.deleteProjectProfileUrl, projectProfileDeleted).toPromise()
+        return this.http.delete(this.projectProfileUrl, new RequestOptions({
+            headers: this.headers,
+            body: projectProfileDeleted
+         })).toPromise()
             .then(res => res.json() as number);
     }
 
     updateProjectProfile(projectProfileUpdated: ProjectProfile): Promise<number> {
-        return this.http.put(this.updateProjectProfileUrl, projectProfileUpdated).toPromise()
+        return this.http.put(this.projectProfileUrl, projectProfileUpdated).toPromise()
             .then(res => res.json() as number);
     }
 }
